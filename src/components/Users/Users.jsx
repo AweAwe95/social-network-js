b1d8ac4101383897ac80e0423d38f354ff3e5335
AweAwe95 <vitalikarach95@gmail.com>
2021-08-16 14:02:41 +0300

    refactor get request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,14 +2,17 @@ import us from './Users.module.css'
 import axios from "axios";
 
 export function Users(props) {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        })
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div className={us.content}>
+            <button onClick={getUsers}>Users</button>
             {
                 props.users.map(user => <div key={user.id}>
                     <div>
